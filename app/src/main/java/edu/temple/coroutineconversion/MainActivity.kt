39d68e687a4e6e232b1d97663799054b46cec1ea
plasticono <friedboy@comcast.net>
2025-04-02 15:33:49 -0400

    Completed

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -7,6 +7,11 @@ import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
@@ -21,24 +26,31 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.currentTextView)
     }
 
-    val handler = Handler(Looper.getMainLooper(), Handler.Callback {
+    /*val handler = Handler(Looper.getMainLooper(), Handler.Callback {
 
         currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it.what)
         cakeImageView.alpha = it.what / 100f
         true
-    })
+    })*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
+            threadTask()
+        }
+    }
+
+   private fun threadTask() {
+        lifecycleScope.launch {
+            repeat(100) { opacity ->
+                withContext(Dispatchers.Main) {
+                    currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", opacity)
+                    cakeImageView.alpha = opacity / 100f
                 }
-            }.start()
+                delay(40L)
+            }
         }
     }
 }
